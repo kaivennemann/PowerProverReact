ee810bde0302023a5b99cb99dcab73d2bb905bd8
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2024-07-16 20:51:07 +0200

    added automatic symbol replacement to search bar input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 
 function GoButton({className}) {
@@ -14,10 +14,69 @@ function GoButton({className}) {
   
 
 function SearchBar() {
+  const [inputValue, setInputValue] = useState('')
+  const inputRef = useRef(null)
+
+  function handleKeyDown(event) {
+    const cursorPos = event.target.selectionStart
+    console.log(inputValue.substring(cursorPos, cursorPos + 2))
+    if (
+      event.key === 'Backspace'
+      && cursorPos < inputValue.length
+      && inputValue.substring(cursorPos - 1, cursorPos + 1) === '()'
+    ) {
+      const newVal = inputValue.substring(0, cursorPos) + inputValue.substring(cursorPos + 2)
+      setInputValue(newVal)
+    }
+  }
+
+  // TODO: add external parentheses when part of input selected
+
+  // TODO: fix cursor moving bug (when in middle of input, cursor jumps)
+
+  // Helpful text replacement for inputing special symbols
+  function replaceText(event) {
+    let curVal = event.target.value
+    const cursorPos = event.target.selectionStart
+
+    if (curVal[cursorPos - 1] === '(') {
+      // Auto-complete parentheses
+      curVal += ')'
+      
+      // Move the cursor back 1 space
+      setTimeout(() => {
+        inputRef.current.setSelectionRange(cursorPos, cursorPos);
+      }, 0)
+    }
+
+    // Ordered text replacements
+    let newVal = [
+      [/not|!/, '¬'],
+      [/ ?(and|&)/, ' ∧ '],
+      [/ ?(or|\|)/, ' ∨ '],
+      [/ ?<\->/, ' ↔ '],
+      [/ ?\->/, ' → '],
+      [/[^a-zA-Z¬∧∨→↔()<\- ]/g, ''] // remove non-letters
+    ].reduce((acc, [old, repl]) => acc.replace(old, repl), curVal)
+
+    // TODO: add spacing around logical symbols
+    // newVal = newVal.replace(/ ?([∧∨→↔]) ?/g, ' $1 ')
+
+    setInputValue(newVal) 
+  }
+
   return (
     <div className='search-bar'>
       <form className='search-form'>
-        <input className='search-bar-input' type='text' placeholder='Enter a formula, e.g. a and b or !c' />
+        <input
+          className='search-bar-input'
+          type='text'
+          placeholder='Enter a formula, e.g. a and b or !c'
+          value={inputValue}
+          onChange={replaceText}
+          onKeyDown={handleKeyDown}
+          ref={inputRef}
+        />
       </form>
       <GoButton className='search-bar-button' />
     </div>
