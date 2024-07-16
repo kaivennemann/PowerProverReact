cdf4e23caafe4d8dfd74d7ae90b42e7bc2800a6c
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2024-07-16 23:40:42 +0200

    added temporary ChatGPT generated background image

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './styles/App.css'
 import QueryContainer from './components/QueryContainer.js'
 import PageHeader from './components/PageHeader.js'
 import PageFooter from './components/PageFooter.js'
+import background from "./static/background.jpeg"
+
 
 const App = () => {
   return (
-    <div className="app">
+    <div className="app" style={{
+      backgroundImage: `url(${background})`,
+      backgroundAttachment: 'fixed',
+      backgroundSize: 'cover'
+    }}>
       <PageHeader className='page-header' />
       <QueryContainer className='query-container' />
       <PageFooter className='page-footer' />
