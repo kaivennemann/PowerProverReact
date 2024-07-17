1806b28fa6ed31c20374a37e14f88b29e0a37ecb
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2024-07-17 23:50:50 +0200

    animated a bunch of inspirational words in random directions

--- src/components/AnimatedBackground.js
+++ src/components/AnimatedBackground.js
@@ -1,20 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/Background.css'
 
 
-function AnimatedText({ className }) {
+function randomAngle() {
+  return Math.random() * 360
+}
+
+function randomDelay() {
+  return Math.random() * 20
+}
+
+function AnimatedText({ text }) {
   return (
-    <div className={className}>
-      AnimatedText
+    <div className='animated-text'>
+      {text}
     </div>
   )
 }
 
+const words = [
+  "Hope",
+  "Courage",
+  "Strength",
+  "Faith",
+  "Dream",
+  "Believe",
+  "Inspire",
+  "Empower",
+  "Resilience",
+  "Perseverance",
+  "Love",
+  "Compassion",
+  "Gratitude"
+]
 
 function AnimatedBackground() {
+  // Adjust angle and delay for each animation
+  useEffect(() => {
+    const textElements = document.querySelectorAll('.animated-text');
+
+    textElements.forEach((element) => {
+      const angle = randomAngle();
+      const delay = randomDelay();
+      element.style.setProperty('--angle', `${angle}deg`);
+      element.style.setProperty('--delay', `${delay}s`);
+    });
+  }, []);
+
   return (
     <div className='animated-background'>
-      <AnimatedText className='animated-text' />
+      {words.map((word, i) => (
+        <AnimatedText text={word} key={i} />
+      ))}
     </div>
   )
 }
