3d49a6ab53188f891f462427a7d8fe1a054e5cb3
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2024-07-21 17:09:24 +0200

    made ast connecting line absolute position dynamic

--- src/components/AbstractSyntaxTree.js
+++ src/components/AbstractSyntaxTree.js
@@ -1,42 +1,51 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useLayoutEffect } from 'react'
 import '../styles/AbstractSyntaxTree.css'
 
 
 function Node({ text }) {
-    return (
-        <div className='ast-node'>
-            {text}
-        </div>
-    )
+  return (
+    <div className='ast-node'>
+      {text}
+    </div>
+  )
 }
 
 function ConnectingLine({ parentNode, childNode }) {
   const [lineStyle, setLineStyle] = useState({})
 
-  useEffect(() => {
+  useLayoutEffect(() => {
+    // position the line so that it connects parent and child nodes
+    const updateLine = () => {
       if (parentNode.current && childNode.current) {
-          const parentRect = parentNode.current.getBoundingClientRect()
-          const childRect = childNode.current.getBoundingClientRect()
-
-          const p = 0.6 // portion of full line that we want to show
-          const x1 = parentRect.left + parentRect.width / 2
-          const y1 = parentRect.top + parentRect.height / 2
-          const x2 = childRect.left + childRect.width / 2
-          const y2 = childRect.top + childRect.height / 2
-          const dx = x2 - x1
-          const dy = y2 - y1
-          const line_x1 = x1 + 0.5 * (1 - p) * dx
-          const line_y1 = y1 + 0.5 * (1 - p) * dy
-          const line_x2 = x2 - 0.5 * (1 - p) * dx
-          const line_y2 = y2 - 0.5 * (1 - p) * dy
-
-          setLineStyle({
-              left: `${line_x1}px`,
-              top: `${line_y1}px`,
-              width: `${Math.sqrt((line_x2 - line_x1) ** 2 + (line_y2 - line_y1) ** 2)}px`,
-              transform: `rotate(${Math.atan2(dy, dx)}rad)`,
-          })
+        const parentRect = parentNode.current.getBoundingClientRect()
+        const childRect = childNode.current.getBoundingClientRect()
+  
+        const p = 0.6 // portion of full line that we want to show
+        const x1 = parentRect.left + parentRect.width / 2
+        const y1 = parentRect.top + parentRect.height / 2
+        const x2 = childRect.left + childRect.width / 2
+        const y2 = childRect.top + childRect.height / 2
+        const dx = x2 - x1
+        const dy = y2 - y1
+        const line_x1 = x1 + 0.5 * (1 - p) * dx
+        const line_y1 = y1 + 0.5 * (1 - p) * dy
+        const line_x2 = x2 - 0.5 * (1 - p) * dx
+        const line_y2 = y2 - 0.5 * (1 - p) * dy
+  
+        setLineStyle({
+            left: `${line_x1}px`,
+            top: `${line_y1}px`,
+            width: `${Math.sqrt((line_x2 - line_x1) ** 2 + (line_y2 - line_y1) ** 2)}px`,
+            transform: `rotate(${Math.atan2(dy, dx)}rad)`,
+        })
       }
+    }
+
+    updateLine()
+    window.addEventListener('resize', updateLine) // ensure absolute position gets adjusted
+
+    // cleanup function
+    return () => window.removeEventListener('resize', updateLine)
   }, [parentNode, childNode])
 
   return (
